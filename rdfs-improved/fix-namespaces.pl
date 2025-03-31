c8985edacc8da14fd67330a75797997219372712
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2025-03-31 21:04:30 +0300

    add couple more namespaces

--- rdfs-improved/fix-namespaces.pl
+++ rdfs-improved/fix-namespaces.pl
@@ -4,8 +4,10 @@
 
 BEGIN {print "\@prefix uml: <http://iec.ch/TC57/NonStandard/UML#>.\n"}
 
-s{http://iec.ch/TC57/CIM100#}         {https://cim.ucaiug.io/ns#};
-s{http://iec.ch/TC57/CIM100-European#}{https://cim.ucaiug.io/ns/eu#};
-s{http://entsoe.eu/ns/nc#}            {https://cim4.eu/ns/nc#};
-s{http://purl.org/dc/terms/#}         {http://purl.org/dc/terms/};
-s{dcterms:}                           {dct:}g;
+s{http://langdale.com.au/2005/UML#}          {http://iec.ch/TC57/NonStandard/UML#};
+s{http://iec.ch/TC57/CIM100#}                {https://cim.ucaiug.io/ns#};
+s{http://iec.ch/TC57/2016/CIM-schema-cim17#} {https://cim.ucaiug.io/ns#};
+s{http://iec.ch/TC57/CIM100-European#}       {https://cim.ucaiug.io/ns/eu#};
+s{http://entsoe.eu/ns/nc#}                   {https://cim4.eu/ns/nc#};
+s{http://purl.org/dc/terms/#}                {http://purl.org/dc/terms/};
+s{dct:}                                      {dcterms:}g;
