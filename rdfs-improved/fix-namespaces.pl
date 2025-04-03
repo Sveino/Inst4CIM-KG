3d603526d4c4034017544f0ca6e5b29b112e1239
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2025-04-03 19:04:35 +0300

    fix duplicated CIM/CIM100 namespace

--- rdfs-improved/fix-namespaces.pl
+++ rdfs-improved/fix-namespaces.pl
@@ -6,6 +6,7 @@ BEGIN {print "\@prefix uml: <http://iec.ch/TC57/NonStandard/UML#>.\n"}
 
 s{http://langdale.com.au/2005/UML#}          {http://iec.ch/TC57/NonStandard/UML#};
 s{http://iec.ch/TC57/CIM100#}                {https://cim.ucaiug.io/ns#};
+s{http://iec.ch/TC57/CIM/CIM100#}            {https://cim.ucaiug.io/ns#}; # bug in Nordic44 AS/AC instance data
 s{http://iec.ch/TC57/2016/CIM-schema-cim17#} {https://cim.ucaiug.io/ns#};
 s{http://iec.ch/TC57/CIM100-European#}       {https://cim.ucaiug.io/ns/eu#};
 s{http://entsoe.eu/ns/nc#}                   {https://cim4.eu/ns/nc#};
